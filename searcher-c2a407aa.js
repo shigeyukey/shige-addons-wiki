de7a88a5478af06b281bcba08359be3a896e6cd4
shigeyukey <shigeyukey@users.noreply.github.com>
2025-12-26 06:40:03 +0000

    deploy: 3711fd60048dcb378278b3a420efd3f0f18398b2

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fab323c5.js',
+                path_to_root + 'searchindex-57292625.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
